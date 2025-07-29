647b6b0d00ba126b1d0a5a975e04ee57c26889e3
krukj <126516292+krukj@users.noreply.github.com>
2025-07-29 14:21:16 +0200

    eda changes

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import re
 import string
-from nltk.corpus import opinion_lexicon
 from sklearn.base import BaseEstimator, TransformerMixin
 from wordcloud import STOPWORDS
+import fasttext
+from huggingface_hub import hf_hub_download
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+import torch.nn.functional as F
+from tqdm import tqdm
 
+tqdm.pandas()
 stopwords = set(STOPWORDS)
 
-POSITIVE_WORDS = set(opinion_lexicon.positive())
-NEGATIVE_WORDS = set(opinion_lexicon.negative())
 
 EMOJI_PATTERN = re.compile(
     "["
@@ -21,124 +25,126 @@ EMOJI_PATTERN = re.compile(
 )
 
 
-class TweetFeatureExtractor(BaseEstimator, TransformerMixin):
+def map_labels(df: pd.DataFrame) -> pd.DataFrame:
     """
-    A class for extracting features from tweets.
+    Maps numerical labels to their corresponding text labels in the given DataFrame.
 
+    Args:
+        df (pd.DataFrame): A pandas DataFrame containing a column named 'label'
+                           with numerical values representing different categories.
+
+    Returns:
+        pd.DataFrame: The original DataFrame with an additional column 'label_text'
+                       that contains the mapped text labels.
+                       The mapping is as follows:
+                       - 0: "Human"
+                       - 1: "Bot"
+    """
+    label_map = {0: "Human", 1: "Bot"}
+    df["label_text"] = df["label"].map(label_map)
+    return df
+
+
+class TweetExploder(BaseEstimator, TransformerMixin):
+    """
+    A transformer that explodes a DataFrame column containing lists of tweets into separate rows.
+
+    Methods:
+        fit(X, y=None):
+            Fits the transformer to the data (no operation in this case).
+
+        transform(X, y=None):
+            Transforms the input DataFrame by exploding the 'tweet' column into separate rows.
+
+    Parameters:
+        X (pd.DataFrame): Input DataFrame containing a column named 'tweet' with lists of tweets.
+        y (optional): Ignored. Exists for compatibility with scikit-learn's pipeline.
+
+    Returns:
+        pd.DataFrame: A new DataFrame with the 'tweet' column exploded into separate rows.
     """
 
     def __init__(self) -> None:
-        pass
+        super().__init__()
 
-    def fit(self, X, y=None) -> "TweetFeatureExtractor":
+    def fit(self, X: pd.DataFrame, y=None) -> "TweetExploder":
+        return self
+
+    def transform(self, X: pd.DataFrame, y=None) -> pd.DataFrame:
+        X_copy = X.copy()
+        return X_copy.explode("tweet").reset_index(drop=True)
+
+
+class TweetFeatureExtractor(BaseEstimator, TransformerMixin):
+    """
+    A transformer to extract features from tweets.
+
+    This transformer adds several new columns to the input DataFrame based on the content of the 'tweet' column.
+    The new columns include:
+        - word_count: The number of words in the tweet.
+        - character_count: The total number of characters in the tweet.
+        - hashtag_count: The number of hashtags present in the tweet.
+        - mention_count: The number of mentions (user tags) in the tweet.
+        - link_count: The number of links present in the tweet.
+        - emoji_count: The number of emojis present in the tweet.
+
+    Attributes:
+        None
+
+    Methods:
+       fit(X, y=None):
+            Fits the transformer to the data (no operation in this case).
+
+        transform(X):
+            Transforms the input DataFrame by adding new feature columns.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def fit(self, X: pd.DataFrame, y=None) -> "TweetFeatureExtractor":
         return self
 
     def transform(self, X: pd.DataFrame) -> pd.DataFrame:
-        """
-        Adds new columns ('avg_word_count', 'avg_character_count', 'avg_hashtag_count', 'avg_mention_count',
-        'avg_link_count', 'avg_emoji_count', 'avg_positive_word_count', 'avg_negative_word_count') to X based on column 'tweet'.
-
-        Args:
-            X (pd.DataFrame): A pandas DataFrame containing column 'tweet'.
-
-        Returns:
-            pd.DataFrame: A modified X with new added columns.
-        """
         X_copy = X.copy()
-        X_copy["tweets_joined"] = X_copy["tweet"].apply(
-            lambda tweet_list: (
-                " [SEP] ".join(str(t) for t in tweet_list)
-                if isinstance(tweet_list, list)
-                else ""
-            )
+
+        # word count
+        X_copy["word_count"] = X_copy["tweet"].apply(lambda tweet: len(tweet.split()))
+
+        # character count
+        X_copy["character_count"] = X_copy["tweet"].apply(lambda tweet: len(tweet))
+
+        # hashtag count
+        X_copy["hashtag_count"] = X_copy["tweet"].apply(lambda tweet: tweet.count("#"))
+
+        #  @ count
+        X_copy["mention_count"] = X_copy["tweet"].apply(lambda tweet: tweet.count("@"))
+
+        # links count
+        X_copy["link_count"] = X_copy["tweet"].apply(lambda tweet: tweet.count("https"))
+
+        # emoji count
+        X_copy["emoji_count"] = X_copy["tweet"].apply(
+            lambda tweet: len(EMOJI_PATTERN.findall(tweet))
         )
 
-        # average word count
-        X_copy["avg_word_count"] = X_copy["tweet"].apply(
-            lambda tweet_list: (
-                sum(len(tweet.split()) for tweet in tweet_list) / len(tweet_list)
-                if isinstance(tweet_list, list) and len(tweet_list) > 0
-                else 0
-            )
-        )
-
-        # average character count
-        X_copy["avg_character_count"] = X_copy["tweet"].apply(
-            lambda tweet_list: (
-                sum(len(tweet) for tweet in tweet_list) / len(tweet_list)
-                if isinstance(tweet_list, list) and len(tweet_list) > 0
-                else 0
-            )
-        )
-
-        # average hashtag count
-        X_copy["avg_hashtag_count"] = X_copy["tweet"].apply(
-            lambda tweet_list: (
-                sum(tweet.count("#") for tweet in tweet_list) / len(tweet_list)
-                if isinstance(tweet_list, list) and len(tweet_list) > 0
-                else 0
-            )
-        )
-
-        # average @ count
-        X_copy["avg_mention_count"] = X_copy["tweet"].apply(
-            lambda tweet_list: (
-                sum(tweet.count("@") for tweet in tweet_list) / len(tweet_list)
-                if isinstance(tweet_list, list) and len(tweet_list) > 0
-                else 0
-            )
-        )
-
-        # average links count
-        X_copy["avg_link_count"] = X_copy["tweet"].apply(
-            lambda tweet_list: (
-                sum(tweet.count("https") for tweet in tweet_list) / len(tweet_list)
-                if isinstance(tweet_list, list) and len(tweet_list) > 0
-                else 0
-            )
-        )
-
-        # average emoji count
-        X_copy["avg_emoji_count"] = X_copy["tweet"].apply(
-            lambda tweet_list: (
-                sum(len(EMOJI_PATTERN.findall(tweet)) for tweet in tweet_list)
-                / len(tweet_list)
-                if isinstance(tweet_list, list) and len(tweet_list) > 0
-                else 0
-            )
-        )
-
-        # average positive words count
-        X_copy["avg_positive_word_count"] = X_copy["tweet"].apply(
-            lambda tweet_list: (
-                sum(
-                    sum(1 for w in tweet.split() if w.lower() in POSITIVE_WORDS)
-                    for tweet in tweet_list
-                )
-                / len(tweet_list)
-                if isinstance(tweet_list, list) and len(tweet_list) > 0
-                else 0
-            )
-        )
-
-        # average negative words count
-        X_copy["avg_negative_word_count"] = X_copy["tweet"].apply(
-            lambda tweet_list: (
-                sum(
-                    sum(1 for w in tweet.split() if w.lower() in NEGATIVE_WORDS)
-                    for tweet in tweet_list
-                )
-                / len(tweet_list)
-                if isinstance(tweet_list, list) and len(tweet_list) > 0
-                else 0
-            )
-        )
         return X_copy
 
 
 class TweetProcessor(BaseEstimator, TransformerMixin):
     """
-    A class for preprocessing tweets.
+    A transformer to preprocess tweets.
+
+    Attributes:
+        None
+
+    Methods:
+        fit(X, y=None):
+            Fits the transformer to the data (does nothing in this case).
+
+        transform(X):
+            Preprocesses the tweets in the provided DataFrame and returns a new DataFrame
+            with the cleaned tweets.
     """
 
     def __init__(self) -> None:
@@ -148,57 +154,177 @@ class TweetProcessor(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X: pd.DataFrame) -> pd.DataFrame:
-        """
-        Preprocesses tweets (changes to lowercase, removes links, emojis, hashtag symbol, removes punctuation marks etc.).
-
-        Args:
-            X (pd.DataFrame): A pandas DataFrame containing column 'tweet'.
-
-        Returns:
-            pd.DataFrame: A pandas DataFrame with preprocessed tweets.
-        """
         X_copy = X.copy()
 
-        # join user's tweets
-        X_copy["tweets_joined"] = X_copy["tweet"].apply(
-            lambda tweet_list: (
-                " [SEP] ".join(tweet_list) if isinstance(tweet_list, list) else " "
-            )
-        )
-
         # lowercase
-        X_copy["tweets_joined"] = X_copy["tweets_joined"].str.lower()
+        X_copy["tweet"] = X_copy["tweet"].str.lower()
 
         # remove links -> [LINK]
-        X_copy["tweets_joined"] = X_copy["tweets_joined"].apply(
+        X_copy["tweet"] = X_copy["tweet"].apply(
             lambda tweet: re.sub(r"http\S+|www\S+|https\S+", "[LINK]", tweet)
         )
         # remove mentions -> [USER]
-        X_copy["tweets_joined"] = X_copy["tweets_joined"].apply(
+        X_copy["tweet"] = X_copy["tweet"].apply(
             lambda tweet: re.sub(r"@\w+", "[USER]", tweet)
         )
 
         # remove '#' from hashtags
-        X_copy["tweets_joined"] = X_copy["tweets_joined"].apply(
+        X_copy["tweet"] = X_copy["tweet"].apply(
             lambda tweet: re.sub(r"#(\w+)", r"\1", tweet)
         )
 
         # remove emojis -> [EMOJI]
-        X_copy["tweets_joined"] = X_copy["tweets_joined"].apply(
+        X_copy["tweet"] = X_copy["tweet"].apply(
             lambda tweet: EMOJI_PATTERN.sub("[EMOJI]", tweet)
         )
 
         # remove punctuation
         punctuation_pattern = f"[{re.escape(string.punctuation)}]"
-        X_copy["tweets_joined"] = X_copy["tweets_joined"].apply(
+        X_copy["tweet"] = X_copy["tweet"].apply(
             lambda tweet: re.sub(punctuation_pattern, "", tweet)
         )
 
         # remove stop words
-        X_copy["tweets_joined"] = X_copy["tweets_joined"].apply(
+        X_copy["tweet"] = X_copy["tweet"].apply(
             lambda tweet: " ".join(
                 [word for word in tweet.split() if word not in stopwords]
             )
         )
 
+        return X_copy
+
+
+class LanguageDetector(BaseEstimator, TransformerMixin):
+    """
+    A transformer for detecting tweet language using fastText.
+
+    Attributes:
+        model (fasttext.FastText): The fastText model loaded for language detection.
+
+    Methods:
+        fit(X, y=None):
+            Fits the transformer. This method does not perform any fitting as the model is pre-trained.
+
+        transform(X):
+            Transforms the input DataFrame by adding a new column 'language' that contains
+            the detected language for each tweet.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        model_path = hf_hub_download(
+            repo_id="facebook/fasttext-language-identification", filename="model.bin"
+        )
+        self.model = fasttext.load_model(model_path)
+
+    def fit(self, X: pd.DataFrame, y=None) -> "LanguageDetector":
+        return self
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        X_copy = X.copy()
+
+        X_copy["language"] = X_copy["tweet"].progress_apply(
+            lambda tweet: (
+                self.model.predict(tweet.replace("\n", " ").replace("\r", " "))[0][
+                    0
+                ].replace("__label__", "")
+                if isinstance(tweet, str) and tweet.strip()
+                else "unknown"
+            )
+        )
+
+        return X_copy
+
+
+class EnglishSelector(BaseEstimator, TransformerMixin):
+    """
+    A transformer that selects rows from a DataFrame where the 'language' column
+    is equal to 'eng_Latn'.
+
+    Attributes:
+        None
+
+    Methods:
+        fit(X, y=None):
+            Fits the transformer. This method does not perform any operation
+            and simply returns the instance.
+
+        transform(X):
+            Transforms the input DataFrame by selecting only the rows where
+            the 'language' column is 'eng_Latn'.
+
+    Parameters:
+        X (pd.DataFrame): The input DataFrame containing a 'language' column.
+        y (optional): Ignored, exists for compatibility with scikit-learn.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing only the rows with 'language'
+        equal to 'eng_Latn'.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def fit(self, X: pd.DataFrame, y=None) -> "EnglishSelector":
+        return self
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        X_copy = X.copy()
+        return X_copy[X_copy["language"] == "eng_Latn"]
+
+
+class SentimentScorer(BaseEstimator, TransformerMixin):
+    """
+    A transformer for scoring the sentiment of tweets using a pre-trained model.
+
+    This class utilizes the `cardiffnlp/twitter-xlm-roberta-base-sentiment` model
+    to classify tweets into three sentiment categories: negative, neutral, and positive.
+
+    Attributes:
+        model: A pre-trained sequence classification model.
+        tokenizer: A tokenizer for processing input text.
+
+    Methods:
+        fit(X, y=None):
+            Fits the transformer to the data (no operation in this case).
+
+        transform(X):
+            Transforms the input DataFrame by adding a 'sentiment' column
+            with the predicted sentiment for each tweet.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        model_path = "cardiffnlp/twitter-xlm-roberta-base-sentiment"
+        self.model = AutoModelForSequenceClassification.from_pretrained(model_path)
+        self.model.eval()
+        self.tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=True)
+
+    def fit(self, X: pd.DataFrame, y=None) -> "SentimentScorer":
+        return self
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        X_copy = X.copy()
+
+        def get_sentiment(tweet):
+            if not isinstance(tweet, str) or tweet.strip() == "":
+                return "neutral"
+
+            inputs = self.tokenizer(
+                tweet,
+                return_tensors="pt",
+                truncation=True,
+                padding=True,
+                max_length=512,
+            )
+            with torch.no_grad():
+                outputs = self.model(**inputs)
+                probs = F.softmax(outputs.logits, dim=1)
+                label = torch.argmax(probs).item()
+
+                # negative = 0, neutral = 1, positive = 2
+                labels = ["negative", "neutral", "positive"]
+                return labels[label]
+
+        X_copy["sentiment"] = X_copy["tweet"].progress_apply(get_sentiment)
         return X_copy
--- src/visualizations.py
+++ src/visualizations.py
@@ -4,9 +4,10 @@ import pandas as pd
 from wordcloud import STOPWORDS
 from collections import Counter
 
-COLOR = "#4C72B0"
+COLOR = "#1e96fc"
 ALPHA = 0.8
-PALETTE = ["#4C72B0", "#55A868"]
+ALPHA_HIST = 0.7
+PALETTE = ["#1e96fc", "#ffb703"]
 LABEL_FONTSIZE = 14
 TITLE_FONTSIZE = 16
 TICKS_FONTSIZE = 12
@@ -21,10 +22,9 @@ def plot_label_distribution(df: pd.DataFrame) -> None:
     Plots label distribution of df.
 
     Args:
-        df (pd.DataFrame): A pandas DataFrame containing column 'label' with values 0 and 1.
+        df (pd.DataFrame): A pandas DataFrame containing column 'label_text'.
     """
-    label_map = {0: "Human", 1: "Bot"}
-    counts = df["label"].map(label_map).value_counts()
+    counts = df["label_text"].value_counts()
     total_count = sum(counts.values)
     _, ax = plt.subplots(figsize=FIGSIZE)
     bars = ax.bar(
@@ -64,16 +64,14 @@ def plot_most_common_words(
     all_words = []
     stopwords = set(STOPWORDS)
 
-    for tweet_list in df["tweet"]:
-        if isinstance(tweet_list, list):
-            for tweet in tweet_list:
-                if isinstance(tweet, str):
-                    words = tweet.lower().split()
-                    if include_stopwords:
-                        all_words.extend(words)
-                    else:
-                        filtered_words = [w for w in words if w not in stopwords]
-                        all_words.extend(filtered_words)
+    for tweet in df["tweet"]:
+        if isinstance(tweet, str):
+            words = tweet.lower().split()
+            if include_stopwords:
+                all_words.extend(words)
+            else:
+                filtered_words = [w for w in words if w not in stopwords]
+                all_words.extend(filtered_words)
     counter = Counter(all_words)
     most_common_words = counter.most_common(n)
     words, counts = zip(*most_common_words)
@@ -105,37 +103,36 @@ def plot_most_common_words(
 
 def plot_histograms(df: pd.DataFrame, by_label: bool) -> None:
     """
-    Plots histograms of 8 columns: avg_word_count, avg_character_count, avg_hashtag_count, avg_mention_count, avg_link_count,
-    avg_emoji_count, avg_positive_word_count, avg_negative_word_count.
+    Plots histograms of 6 columns: word_count, character_count, hashtag_count, mention_count, link_count,
+    emoji_count.
 
     Args:
-        df (pd.DataFrame): A pandas DataFrame containing columns 'avg_word_count', 'avg_character_count', 'avg_hashtag_count', 'avg_mention_count', 'avg_link_count',
-        'avg_emoji_count', 'avg_positive_word_count', 'avg_negative_word_count' and 'label'.
+        df (pd.DataFrame): A pandas DataFrame containing columns 'word_count', 'character_count', 'hashtag_count', 'mention_count', 'link_count',
+        'emoji_count' and 'label_text'.
         by_label (bool): Whether to group columns by label.
     """
     cols_to_plot = [
-        "avg_word_count",
-        "avg_character_count",
-        "avg_hashtag_count",
-        "avg_mention_count",
-        "avg_link_count",
-        "avg_emoji_count",
-        "avg_positive_word_count",
-        "avg_negative_word_count",
+        "word_count",
+        "character_count",
+        "hashtag_count",
+        "mention_count",
+        "link_count",
+        "emoji_count",
     ]
-    _, axes = plt.subplots(4, 2, figsize=SUBPLOTS_FIGSIZE)
+    _, axes = plt.subplots(int(len(cols_to_plot) / 2), 2, figsize=SUBPLOTS_FIGSIZE)
     if by_label:
         for col, ax in zip(cols_to_plot, axes.flatten()):
             sns.histplot(
                 data=df,
                 x=col,
-                hue="label",
+                hue="label_text",
                 bins=30,
                 palette=PALETTE,
                 edgecolor="black",
-                alpha=ALPHA,
-                common_norm=False,
+                alpha=ALPHA_HIST,
+                common_norm=True,
                 ax=ax,
+                legend=True,
             )
             ax.set_xlabel(col)
             ax.set_ylabel("Count")
@@ -147,8 +144,8 @@ def plot_histograms(df: pd.DataFrame, by_label: bool) -> None:
                 x=col,
                 bins=30,
                 edgecolor="black",
-                alpha=ALPHA,
-                common_norm=False,
+                alpha=ALPHA_HIST,
+                common_norm=True,
                 ax=ax,
             )
     plt.tight_layout()
@@ -160,25 +157,20 @@ def plot_most_common_words_per_label(df: pd.DataFrame, label: str, n: int = 10) 
     Plots n most common words per label (bot or human).
 
     Args:
-        df (pd.DataFrame): A pandas DataFrame containing columns 'tweet' and 'label'.
+        df (pd.DataFrame): A pandas DataFrame containing columns 'tweet' and 'label_text'.
         label (str): Label to be displayed ('bot' or 'human').
         n (int, optional): Number of most common words to be displayed. Defaults to 10.
     """
-    if label == "human":
-        df = df[df["label"] == 0]
-    elif label == "bot":
-        df = df[df["label"] == 1]
+    df = df[df["label_text"] == label.capitalize()]
 
     all_words = []
     stopwords = set(STOPWORDS)
 
-    for tweet_list in df["tweet"]:
-        if isinstance(tweet_list, list):
-            for tweet in tweet_list:
-                if isinstance(tweet, str):
-                    words = tweet.lower().split()
-                    filtered_words = [w for w in words if w not in stopwords]
-                    all_words.extend(filtered_words)
+    for tweet in df["tweet"]:
+        if isinstance(tweet, str):
+            words = tweet.lower().split()
+            filtered_words = [w for w in words if w not in stopwords]
+            all_words.extend(filtered_words)
     counter = Counter(all_words)
     most_common_words = counter.most_common(n)
     words, counts = zip(*most_common_words)
@@ -206,6 +198,7 @@ def plot_most_common_words_per_label(df: pd.DataFrame, label: str, n: int = 10) 
     plt.xticks(rotation=ROTATION, fontsize=TICKS_FONTSIZE)
     plt.show()
 
+
 def plot_column_vs_label(df: pd.DataFrame, column: str) -> None:
     """
     Plots the distribution of bot and human labels within any boolean column (e.g., verified, default_profile).
@@ -215,31 +208,116 @@ def plot_column_vs_label(df: pd.DataFrame, column: str) -> None:
         column (str): Name of the boolean column to group by.
     """
 
-    label_map = {0: 'Human', 1: 'Bot'}
-    value_map = {True: 'Yes', False: 'No'}
+    label_map = {0: "Human", 1: "Bot"}
+    value_map = {True: "Yes", False: "No"}
 
     df_copy = df.copy()
-    df_copy['label_text'] = df_copy['label'].map(label_map)
+    df_copy["label_text"] = df_copy["label"].map(label_map)
 
-    df_copy[column] = df_copy[column].map({'True ': True, 'False ': False})
-    df_copy['column_text'] = df_copy[column].map(value_map)
+    df_copy[column] = df_copy[column].map({"True ": True, "False ": False})
+    df_copy["column_text"] = df_copy[column].map(value_map)
 
     _, ax = plt.subplots(figsize=FIGSIZE)
     sns.histplot(
         data=df_copy,
-        x='column_text',
-        hue='label_text',
-        multiple='fill',
+        x="column_text",
+        hue="label_text",
+        multiple="fill",
         shrink=0.8,
         palette=PALETTE,
-        edgecolor='black',
+        edgecolor="black",
         alpha=ALPHA,
-        ax=ax
+        ax=ax,
     )
 
-    ax.set_ylabel('Proportion')
-    ax.set_xlabel(column.replace('_', ' ').capitalize())
-    ax.set_title(f'Label Distribution by {column.replace("_", " ").capitalize()}', fontsize=TITLE_FONTSIZE)
+    ax.set_ylabel("Proportion")
+    ax.set_xlabel(column.replace("_", " ").capitalize())
+    ax.set_title(
+        f'Label Distribution by {column.replace("_", " ").capitalize()}',
+        fontsize=TITLE_FONTSIZE,
+    )
     plt.xticks(fontsize=TICKS_FONTSIZE)
     plt.tight_layout()
+    plt.show()
+
+
+def plot_top_languages(df: pd.DataFrame, n: int = 10) -> None:
+    """
+    Plots n most common languages of tweets.
+
+    Args:
+        df (pd.DataFrame): A pandas DataFrame containing column 'language'.
+        n (int, optional): Number of languages to be displayed. Defaults to 10.
+    """
+    counts = df["language"].value_counts().head(n=n)
+    total_count = sum(counts.values)
+
+    _, ax = plt.subplots(figsize=FIGSIZE)
+    bars = ax.bar(
+        counts.index, 100 * counts.values / total_count, alpha=ALPHA, color=COLOR
+    )
+    for bar in bars:
+        height = bar.get_height()
+        ax.annotate(
+            f"{height:.1f}%",
+            xy=(bar.get_x() + bar.get_width() / 2, height),
+            xytext=(0, 3),
+            textcoords="offset points",
+            ha="center",
+            va="bottom",
+            fontsize=BAR_FONTSIZE,
+        )
+
+    ax.set_xlabel("Language", fontsize=LABEL_FONTSIZE)
+    ax.set_ylabel("Percentage (%)", fontsize=LABEL_FONTSIZE)
+    ax.set_title(f"Top {n} languages by percentage", fontsize=TITLE_FONTSIZE)
+    plt.xticks(rotation=ROTATION, fontsize=TICKS_FONTSIZE)
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_sentiment_vs_label(df: pd.DataFrame, sample: bool = True) -> None:
+    counts = pd.crosstab(df["label_text"], df["sentiment"])
+
+    percentages = counts.div(counts.sum(axis=1), axis=0) * 100
+    percentages = percentages.round(1)
+
+    sns.heatmap(
+        percentages, annot=True, fmt=".1f", cmap="YlGnBu", cbar_kws={"label": "%"}
+    )
+    plt.title("Percentage of sentiment by labels" + " in sample" if sample else "")
+    plt.xlabel("Sentiment", fontsize=LABEL_FONTSIZE)
+    plt.ylabel("Label", fontsize=LABEL_FONTSIZE)
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_feature_correlation_with_labels(
+    df: pd.DataFrame, feature_cols: list[str]
+) -> None:
+    """
+    Plots a heatmap showing correlation between labels and numerical features.
+
+    Args:
+        df (pd.DataFrame): A pandas DataFrame containing 'label' and numerical feature columns.
+        feature_cols (list): List of column names to correlate with labels.
+    """
+
+    combined = df[["label"] + feature_cols]
+    correlation_matrix = combined.corr().loc[["label"], feature_cols]
+
+    plt.figure(figsize=FIGSIZE)
+    sns.heatmap(
+        correlation_matrix,
+        annot=True,
+        cmap="coolwarm",
+        center=0,
+        linewidths=0.5,
+        fmt=".2f",
+    )
+    plt.title("Correlation of features with Bot label", fontsize=TITLE_FONTSIZE)
+    plt.xlabel("Feature", fontsize=LABEL_FONTSIZE)
+    plt.ylabel("Label", fontsize=LABEL_FONTSIZE)
+    plt.tight_layout()
+    plt.show()
     plt.show()
